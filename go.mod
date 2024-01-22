fef0c0b3416a1c429cbb62d118c82aeec27c7516
Frank Mayer <gh@frankmayer.dev>
2024-01-22 17:11:37 +0100

    temp

--- go.mod
+++ go.mod
@@ -20,14 +20,20 @@ require (
 	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/ProtonMail/go-crypto v0.0.0-20230828082145-3c4c8a2d2371 // indirect
 	github.com/alecthomas/chroma v0.10.0 // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/bodgit/plumbing v1.2.0 // indirect
+	github.com/bodgit/sevenzip v1.3.0 // indirect
+	github.com/bodgit/windows v1.0.0 // indirect
 	github.com/charmbracelet/lipgloss v0.9.1 // indirect
 	github.com/cloudflare/circl v1.3.7 // indirect
+	github.com/connesc/cipherio v0.2.1 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/cyphar/filepath-securejoin v0.2.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dlclark/regexp2 v1.4.0 // indirect
+	github.com/dsnet/compress v0.0.1 // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/fredbi/uri v1.0.0 // indirect
 	github.com/fyne-io/gl-js v0.0.0-20220119005834-d2da28d9ccfe // indirect
@@ -42,21 +48,29 @@ require (
 	github.com/go-text/typesetting v0.0.0-20230616162802-9c17dd34aa4a // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/klauspost/pgzip v1.2.5 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mholt/archiver/v4 v4.0.0-alpha.8 // indirect
 	github.com/microcosm-cc/bluemonday v1.0.26 // indirect
 	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/nwaples/rardecode/v2 v2.0.0-beta.2 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/pjbgf/sha1cd v0.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
@@ -66,9 +80,12 @@ require (
 	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/tevino/abool v1.2.0 // indirect
+	github.com/therootcompany/xz v1.0.1 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	github.com/yuin/goldmark v1.6.0 // indirect
 	github.com/yuin/goldmark-emoji v1.0.1 // indirect
+	go4.org v0.0.0-20200411211856-f5505b9728dd // indirect
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/image v0.15.0 // indirect
--- internal/util/unzip.go
+++ internal/util/unzip.go
@@ -1,197 +1,49 @@
 package util
 
 import (
-	"archive/tar"
-	"archive/zip"
-	"compress/gzip"
+	"context"
 	"errors"
 	"fmt"
 	"io"
 	"os"
-	"os/exec"
 	"path/filepath"
 
 	"github.com/charmbracelet/log"
+	"github.com/mholt/archiver/v4"
 )
+
+var current = []string{"."}
 
 // Unzip unzips an archive at its location
 func Unzip(p string) error {
-	log.Debug("Unzipping", "filepath", p)
-	ext := filepath.Ext(p)
-	switch ext {
-	case ".zip":
-		return unzip(p, filepath.Dir(p))
-	case ".tar":
-		r, err := os.Open(p)
-		if err != nil {
-			return err
-		}
-		defer r.Close()
-		return untar(r, filepath.Dir(p))
-	case ".gz":
-		r, err := os.Open(p)
-		if err != nil {
-			return err
-		}
-		defer r.Close()
-		gzipReader, err := gzip.NewReader(r)
-		if err != nil {
-			return err
-		}
-		defer gzipReader.Close()
-		return untar(gzipReader, filepath.Dir(p))
-	case ".xz":
-		return unxz(p, filepath.Dir(p))
-	default:
-		return fmt.Errorf("Unknown archive type '%s'", ext)
-	}
-}
-
-// unzip unzips a zip archive at its location
-func unzip(archPath string, outPath string) error {
-	log.Debug("Unzipping", "outPath", outPath)
-	r, err := zip.OpenReader(archPath)
+	target := filepath.Dir(p)
+	log.Debug("Unzipping", "filepath", p, "target", target)
+	fstream, err := os.Open(p)
+	format, reader, err := archiver.Identify(p, fstream)
 	if err != nil {
 		return errors.Join(
-			fmt.Errorf("Error opening zip file '%s'", archPath),
+			fmt.Errorf("Error opening archive '%s'", p),
 			err,
 		)
 	}
-	defer r.Close()
 
-	for _, f := range r.File {
-		rc, err := f.Open()
+	ctx := context.Background()
+
+	if ex, ok := format.(archiver.Extractor); ok {
+		ex.Extract(ctx, reader, current, func(ctx context.Context, f archiver.File) error {
+			log.Debug("Extracting", "file", f.Name())
+			return nil
+		})
+	}
+
+	if decom, ok := format.(archiver.Decompressor); ok {
+		rc, err := decom.OpenReader(reader)
 		if err != nil {
-			return errors.Join(
-				fmt.Errorf("Error opening file '%s' in zip file '%s'", f.Name, archPath),
-				err,
-			)
+			return errors.Join(fmt.Errorf("Error opening decompressor for '%s'", p), err)
 		}
 		defer rc.Close()
 
-		p := filepath.Join(outPath, f.Name)
-
-		if err := os.MkdirAll(filepath.Dir(p), 0777); err != nil {
-			return errors.Join(
-				fmt.Errorf("Error creating directory '%s'", filepath.Dir(p)),
-				err,
-			)
-		}
-
-		if f.FileInfo().IsDir() {
-			continue
-		}
-
-		file, err := os.OpenFile(p, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode()|0666)
-		if err != nil {
-			return errors.Join(
-				fmt.Errorf("Error creating file '%s'", p),
-				err,
-			)
-		}
-		defer file.Close()
-
-		if _, err = io.Copy(file, rc); err != nil {
-			return errors.Join(
-				fmt.Errorf("Error writing to file '%s'", p),
-				err,
-			)
-		}
 	}
 
 	return nil
-}
-
-// untar untars a tar archive at its location
-func untar(tarStream io.Reader, outPath string) error {
-	log.Debug("Untarring", "outPath", outPath)
-	tarReader := tar.NewReader(tarStream)
-
-	for {
-		header, err := tarReader.Next()
-
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			return errors.Join(
-				fmt.Errorf("Error reading tar file"),
-				err,
-			)
-		}
-
-		p := filepath.Join(outPath, header.Name)
-
-		switch header.Typeflag {
-		case tar.TypeDir:
-			if err := os.MkdirAll(p, 0777); err != nil {
-				return errors.Join(
-					fmt.Errorf("Error creating directory '%s'", header.Name),
-					err,
-				)
-			}
-		case tar.TypeReg:
-			if err := os.MkdirAll(filepath.Dir(p), 0777); err != nil {
-				return errors.Join(
-					fmt.Errorf("Error creating directory '%s'", filepath.Dir(p)),
-					err,
-				)
-			}
-			outFile, err := os.OpenFile(p, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, header.FileInfo().Mode()|0666)
-			if err != nil {
-				return errors.Join(
-					fmt.Errorf("Error creating file '%s'", p),
-					err,
-				)
-			}
-			if _, err := io.Copy(outFile, tarReader); err != nil {
-				return errors.Join(
-					fmt.Errorf("Error writing to file '%s'", p),
-					err,
-				)
-			}
-			// set executable bit
-			if err := os.Chmod(p, header.FileInfo().Mode()); err != nil {
-				return errors.Join(
-					fmt.Errorf("Error setting executable bit on '%s'", p),
-					err,
-				)
-			}
-			outFile.Close()
-		case tar.TypeSymlink:
-			if err := os.MkdirAll(filepath.Dir(p), 0777); err != nil {
-				return errors.Join(
-					fmt.Errorf("Error creating directory '%s'", filepath.Dir(p)),
-					err,
-				)
-			}
-			if err := os.Symlink(header.Linkname, p); err != nil {
-				return errors.Join(
-					fmt.Errorf("Error creating symlink '%s'", p),
-					err,
-				)
-			}
-		default:
-			return errors.Join(
-				fmt.Errorf("Unable to untar type : %c in file %s", header.Typeflag, header.Name),
-				err,
-			)
-		}
-	}
-
-	return nil
-}
-
-// unxz unxzs an .tar.xz archive at its location
-func unxz(p string, outPath string) error {
-	// is GNU tar available?
-	if tar, err := exec.LookPath("tar"); err == nil {
-		cmd := exec.Command(tar, "-xJf", p, "-C", outPath)
-		if err := cmd.Run(); err != nil {
-			return err
-		}
-		return nil
-	}
-	return fmt.Errorf("GNU tar not found")
 }
