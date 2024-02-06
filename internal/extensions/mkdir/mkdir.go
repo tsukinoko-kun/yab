464e8fa958e33e86491c8753ea96c6b27c068459
Frank Mayer <gh@frankmayer.dev>
2024-02-06 23:58:16 +0100

    added mkdir and rm functions (#27)

---
+++ internal/extensions/mkdir/mkdir.go
@@ -0,0 +1,16 @@
+package mkdir
+
+import (
+	"os"
+
+	lua "github.com/Frank-Mayer/gopher-lua"
+)
+
+func Mkdir(l *lua.LState) int {
+	dir := l.CheckString(1)
+	err := os.Mkdir(dir, 0755)
+	if err != nil {
+		l.RaiseError("Error creating directory: %s", err)
+	}
+	return 0
+}
