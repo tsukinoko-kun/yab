977b6e96f38ba6555fc95dc96fb147822aefdf07
Frank Mayer <gh@frankmayer.dev>
2024-02-23 12:51:40 +0100

    goreleaser

--- package.go
+++
@@ -1,6 +0,0 @@
-package yab
-
-const PackageName = "Yab"
-const PackageVersion = "0.2.1"
-const PackageAuthors = "Frank Mayer"
-const PackageCopyRight = PackageName + " " + PackageVersion + " Copyright (C) 2024 " + PackageAuthors
